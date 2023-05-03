d0301684ecabeb0ed92e40383357e80c993b8267
parixscm <parixscm@gmail.com>
2023-05-03 14:39:59 +0900

    🚑 서버: pin 스키마 수정

--- server/Models/pin.js
+++ server/Models/pin.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const pinSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
-    title: { type: String, required: true, minLength: 3 },
-    description: { type: String, required: true, minLength: 3 },
+    place: { type: String, required: true, minLength: 3 },
+    review: { type: String, required: true, minLength: 3 },
     rating: { type: Number, required: true, min: 0, max: 5 },
     lat: { type: Number, required: true },
     long: { type: Number, required: true },
