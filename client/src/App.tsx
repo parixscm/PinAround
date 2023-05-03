f5cf66d43e5100e82f8fe376dabb1e27a4f4c619
parixscm <parixscm@gmail.com>
2023-05-03 14:42:33 +0900

    📝 클라이언트: 화면에 핀 정보 표시, 타입 선언

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,25 @@
 import Map, { Marker, Popup } from "react-map-gl";
 import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+import "moment/locale/ko";
+import { Pins } from "./typings";
 
 function App() {
-  const [showPopup, setShowPopup] = useState(true);
+  const [pins, setPins] = useState<Pins[] | null>(null);
+
+  useEffect(() => {
+    const getAllPins = async () => {
+      try {
+        const response = await axios.get<Pins[]>("/pins");
+        setPins(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAllPins();
+  }, []);
 
   return (
     <Map
@@ -16,42 +32,43 @@ function App() {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker longitude={2.294694} latitude={48.85809} anchor="center">
-        <FaMapMarkerAlt size={"1.8rem"} color="#eb2f06" />
-      </Marker>
-      {showPopup && (
-        <Popup
-          longitude={2.294694}
-          latitude={48.858093}
-          anchor="bottom"
-          closeButton={false}
-          style={{ opacity: "95%" }}
-          onClose={() => setShowPopup(false)}
-          focusAfterOpen={false}
-        >
-          <div className="w-64 h-64 flex flex-col justify-around text-[14px]">
-            <label className="label">장소</label>
-            <h4>에펠탑</h4>
-            <label className="label">리뷰</label>
-            <p>아름다운 장소. 사랑해!</p>
-            <label className="label">별점</label>
-            <div className="flex space-x-0.5 text-yellow-400">
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
+      {pins?.map((pin, idx) => (
+        <div key={idx}>
+          <Marker longitude={pin.long} latitude={pin.lat} anchor="center">
+            <FaMapMarkerAlt size={"1.8rem"} color="#eb2f06" />
+          </Marker>
+
+          <Popup
+            longitude={pin.long}
+            latitude={pin.lat}
+            anchor="bottom"
+            closeButton={false}
+            focusAfterOpen={false}
+          >
+            <div className="w-64 h-64 flex flex-col justify-around text-[14px]">
+              <label className="label">장소</label>
+              <h4>{pin.place}</h4>
+              <label className="label">리뷰</label>
+              <p>{pin.review}</p>
+              <label className="label">별점</label>
+              <div className="flex space-x-0.5 text-yellow-400">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <label className="label">정보</label>
+              <span>
+                작성자: <b>{pin.username}</b>
+              </span>
+              <span className="text-[12px]">
+                작성시간: <b>{moment(pin.createdAt).fromNow()}</b>
+              </span>
             </div>
-            <label className="label">정보</label>
-            <span>
-              작성자: <b>mason</b>
-            </span>
-            <span className="text-[12px]">
-              작성시간: <b>1시간 전</b>
-            </span>
-          </div>
-        </Popup>
-      )}
+          </Popup>
+        </div>
+      ))}
     </Map>
   );
 }
---
+++ client/src/typings.d.ts
@@ -0,0 +1,10 @@
+export type Pins = {
+  username: string;
+  place: string;
+  review: string;
+  rating: number;
+  lat: number;
+  long: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
